bb7ec456b5989f0becf0ad828062b6a370bf269e
First Draft <grades@firstdraft.com>
2025-04-03 01:52:56 +0000

    Update spec/ folder to latest version

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,7 @@
 # Table name: comments
 #
 #  id         :bigint           not null, primary key
-#  body       :text             not null
+#  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  author_id  :bigint           not null
@@ -11,7 +11,8 @@
 #
 # Indexes
 #
-#  index_comments_on_photo_id  (photo_id)
+#  index_comments_on_author_id  (author_id)
+#  index_comments_on_photo_id   (photo_id)
 #
 # Foreign Keys
 #

a77a082b69a6907857b65c7bdf35a6b32d048b0c
wrblr <wrbell@uchicago.edu>
2025-04-03 01:52:08 +0000

    Added enum and created sample data

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -22,4 +22,6 @@
 class FollowRequest < ApplicationRecord
   belongs_to :recipient, class_name: "User"
   belongs_to :sender, class_name: "User"
+
+  enum :status, pending: "pending", rejected: "rejected", accepted: "accepted"
 end

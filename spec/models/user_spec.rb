a5fc6612d59847fd6885efd3ca829546672a2548
First Draft <grades@firstdraft.com>
2025-04-03 01:09:46 +0000

    Update spec/ folder to latest version

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,18 +3,20 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
-#  avatar_image           :string
+#  avatar_image           :string           default("avatar_placeholder.png")
 #  bio                    :string
 #  comments_count         :integer          default(0)
-#  email                  :string           default(""), not null
+#  email                  :citext           default(""), not null
 #  encrypted_password     :string           default(""), not null
+#  integer                :integer          default(0)
 #  likes_count            :integer          default(0)
 #  name                   :string
-#  private                :boolean
+#  photos_count           :integer          default(0)
+#  private                :boolean          default(TRUE)
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
-#  username               :string
+#  username               :citext           not null
 #  website                :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null

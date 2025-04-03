ad9b11daa74fc59a7810289292baaa4dde9008b7
wrblr <wrbell@uchicago.edu>
2025-04-03 01:29:23 +0000

    Added indirect associations to models

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -23,4 +23,5 @@ class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User", counter_cache: true
   has_many :comments
   has_many :likes
+  has_many :fans, through: :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,16 @@ class User < ApplicationRecord
   has_many :own_photos, foreign_key: :owner_id, class_name: "Photo"
   has_many :comments, foreign_key: "author_id"
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :accepted_sent_follow_requests, -> { where(status: "accepted") }, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :accepted_received_follow_requests, -> { where(status: "accepted") }, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :likes, foreign_key: :fan_id
+  has_many :liked_photos, through: :likes, source: :photo
+  has_many :leaders, through: :sent_follow_requests, source: :recipient
+  has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
+  has_many :followers, through: :accepted_received_follow_requests, source: :sender
+
+  has_many :feed, through: :leaders, source: :own_photos
+
+  has_many :discover, -> { distinct }, through: :leaders, source: :liked_photos
 end

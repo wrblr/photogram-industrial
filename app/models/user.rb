3cd9be8fe905e6f30d0853a49398857c2c183447
wrblr <wrbell@uchicago.edu>
2025-04-03 00:52:55 +0000

    Generated photos

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_03_003657) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_03_004705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.string "image"
+    t.integer "comments_count", default: 0
+    t.integer "likes_count", default: 0
+    t.text "caption"
+    t.bigint "owner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_photos_on_owner_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,4 +46,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_03_003657) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
+
+  add_foreign_key "photos", "users", column: "owner_id"
 end

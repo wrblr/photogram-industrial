526594c637fdd41c5599aa75ada8a7fae043e6c8
wrblr <wrbell@uchicago.edu>
2025-04-03 01:24:33 +0000

    Added direct associations to models

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -22,4 +22,5 @@
 class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User", counter_cache: true
   has_many :comments
+  has_many :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
+  has_many :own_photos, foreign_key: :owner_id, class_name: "Photo"
   has_many :comments, foreign_key: "author_id"
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :likes, foreign_key: :fan_id
 end

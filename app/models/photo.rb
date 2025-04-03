212f491b00633252d83a8fc2ac5475253107ae27
wrblr <wrbell@uchicago.edu>
2025-04-03 01:41:57 +0000

    added scope to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -27,4 +27,7 @@ class Photo < ApplicationRecord
 
   validates :caption, presence: true
   validates :image, presence: true
+
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+  scope :by_likes, -> { order(likes_count: :desc) }
 end

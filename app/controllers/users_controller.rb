d4d40d2cb924ad8f9fbf27d5f82f160f70059e01
wrblr <wrbell@uchicago.edu>
2025-04-10 00:01:06 +0000

    posts and liked photos bootstrapped

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by!(username: params.fetch(:username))
   end
+
+  def liked
+    @user = User.find_by!(username: params.fetch(:username))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :photos
   
   get "/:username" => "users#show", as: :user
+  get ":username/liked" => "users#liked", as: :liked
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

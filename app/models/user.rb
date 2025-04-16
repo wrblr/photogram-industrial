d47632b125c074a02b492d2e9b5031fc7513c3de
wrblr <wrbell@uchicago.edu>
2025-04-16 18:43:38 +0000

    Troubleshooting 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,6 +40,7 @@ class User < ApplicationRecord
   has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :pending_received_follow_requests, -> { where(status: 'pending') }, class_name: "FollowRequest", foreign_key: "recipient_id"
   has_many :likes, foreign_key: :fan_id
   has_many :liked_photos, through: :likes, source: :photo
   has_many :leaders, through: :sent_follow_requests, source: :recipient
